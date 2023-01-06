47777cfcc4594cbc642e1ad57d0a506a064fa0e7
Mike Donovan <mike@mikedonovan.dev>
2023-01-06 13:02:22 -0800

    Make nav items clickable

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,20 @@ document.documentElement.style.setProperty('--actual-height', window.innerHeight
 document.body.classList.remove('hidden');
 
 document.getElementById('hamburger').addEventListener('pointerdown', handleHamburgerClick);
+document.getElementById('projects-button').addEventListener('pointerdown', handleNavItemClick);
+document.getElementById('about-button').addEventListener('pointerdown', handleNavItemClick);
+document.getElementById('contact-button').addEventListener('pointerdown', handleNavItemClick);
 
 function handleHamburgerClick() {
   this.classList.toggle('open');
-  document.getElementsByTagName('menu')[0].classList.toggle('open');
+  document.getElementsByTagName('nav')[0].classList.toggle('open');
+}
+
+function handleNavItemClick() {
+  document.getElementById('projects-button').classList.remove('selected');
+  document.getElementById('about-button').classList.remove('selected');
+  document.getElementById('contact-button').classList.remove('selected');
+  this.classList.add('selected');
+  document.getElementsByTagName('nav')[0].classList.toggle('open');
+  document.getElementById('hamburger').classList.toggle('open');
 }
